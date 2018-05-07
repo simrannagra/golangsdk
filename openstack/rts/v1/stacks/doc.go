efba8f0e00f04eb5dbc566355b4b3b2259c4b3e4
d-apurva <apurva.dhoble@click2cloud.net>
2018-05-07 17:26:27 +0530

    added examples in doc.go for stacks and stack templates

--- openstack/rts/v1/stacks/doc.go
+++ openstack/rts/v1/stacks/doc.go
@@ -5,4 +5,16 @@
 // application framework or component specified (in the template). A stack is a
 // running instance of a template. The result of creating a stack is a deployment
 // of the application framework or component.
+
+/*
+Package resources enables management and retrieval of
+RTS service.
+
+Example to List Resources
+
+lis:=stacks.ListOpts{SortDir:stacks.SortAsc,SortKey:stacks.SortStatus}
+	getstack,err:=stacks.List(client,lis).AllPages()
+	stacks,err:=stacks.ExtractStacks(getstack)
+	fmt.Println(stacks)
+*/
 package stacks
