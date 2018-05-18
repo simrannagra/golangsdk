d835afe058f8cfc8514eb40f9d501605ac82a7a5
disha-wani <disha.wani@WSD008>
2018-05-18 13:31:21 +0530

    added v3 clusters code without acceptance

---
+++ openstack/cce/v3/clusters/requests.go
@@ -0,0 +1,99 @@
+package clusters
+
+import "github.com/huaweicloud/golangsdk"
+
+
+var RequestOpts golangsdk.RequestOpts = golangsdk.RequestOpts{
+	MoreHeaders: map[string]string{"Content-Type": "application/json"},
+}
+
+// CreateOptsBuilder allows extensions to add additional parameters to the
+// Create request.
+type CreateOptsBuilder interface {
+	ToClusterCreateMap() (map[string]interface{}, error)
+}
+
+// CreateOpts contains all the values needed to create a new cluster
+type CreateOpts struct {
+	// API type, fixed value Cluster
+	Kind 		string 		`json:"kind" required:"true"`
+	//API version, fixed value v3
+	ApiVersion  string 		`json:"apiversion" required:"true"`
+	//Medata required to create a cluster
+	Metadata   CreateMetaData	`json:"metadata" required:"true"`
+	//specifications to create a cluster
+	Spec		Spec	`json:"spec" required:"true"`
+}
+//Medata required to create a cluster
+type CreateMetaData struct {
+	//Cluster unique name
+	Name 			string 					`json:"name" required:"true"`
+	// Cluster tag, key/value pair format
+	Labels 			[]map[string]string  	`json:"labels,omitempty"`
+	//Cluster annotation, key/value pair format
+	Annotations		[]map[string]string		`json:"annotations,omitempty"`
+}
+
+// ToClusterCreateMap builds a create request body from CreateOpts.
+func (opts CreateOpts) ToClusterCreateMap() (map[string]interface{}, error) {
+	return golangsdk.BuildRequestBody(opts, "")
+}
+
+// Create accepts a CreateOpts struct and uses the values to create a new
+// logical cluster.
+func Create(c *golangsdk.ServiceClient, opts CreateOptsBuilder) (r CreateResult) {
+	b, err := opts.ToClusterCreateMap()
+	if err != nil {
+		r.Err = err
+		return
+	}
+	reqOpt := &golangsdk.RequestOpts{OkCodes: []int{201}}
+	_, r.Err = c.Post(rootURL(c), b, &r.Body, reqOpt)
+	return
+}
+type UpdateOpts struct {
+	Spec		UpdateSpec	`json:"spec" required:"true"`
+}
+type UpdateSpec struct {
+	Description string `json:"description,omitempty"`
+}
+// UpdateOptsBuilder allows extensions to add additional parameters to the
+// Update request.
+type UpdateOptsBuilder interface {
+	ToClusterUpdateMap() (map[string]interface{}, error)
+}
+// ToClusterUpdateMap builds an update body based on UpdateOpts.
+func (opts UpdateOpts) ToClusterUpdateMap() (map[string]interface{}, error) {
+	return golangsdk.BuildRequestBody(opts, "")
+}
+
+// Update allows clusters to update description.
+func Update(c *golangsdk.ServiceClient, id string, opts UpdateOptsBuilder) (r UpdateResult) {
+	b, err := opts.ToClusterUpdateMap()
+	if err != nil {
+		r.Err = err
+		return
+	}
+	_, r.Err = c.Put(resourceURL(c, id), b, &r.Body, &golangsdk.RequestOpts{
+		OkCodes: []int{200},
+	})
+	return
+}
+
+// Delete will permanently delete a particular cluster based on its unique ID.
+func Delete(c *golangsdk.ServiceClient, id string) (r DeleteResult) {
+	_, r.Err = c.Delete(resourceURL(c, id), &golangsdk.RequestOpts{
+		OkCodes:     []int{200},
+		MoreHeaders: RequestOpts.MoreHeaders, JSONBody: nil,
+	})
+	return
+}
+
+// Get retrieves a particular cluster based on its unique ID.
+func Get(c *golangsdk.ServiceClient, id string) (r GetResult) {
+	_, r.Err = c.Get(resourceURL(c, id), &r.Body, &golangsdk.RequestOpts{
+		OkCodes:     []int{200},
+		MoreHeaders: RequestOpts.MoreHeaders, JSONBody: nil,
+	})
+	return
+}
---
+++ openstack/cce/v3/clusters/urls.go
@@ -0,0 +1,15 @@
+package clusters
+
+import "github.com/huaweicloud/golangsdk"
+
+const (
+	rootpath ="clusters"
+)
+
+func rootURL(client *golangsdk.ServiceClient) string {
+	return client.ServiceURL(rootpath)
+}
+func resourceURL(c *golangsdk.ServiceClient, id string) string {
+	return c.ServiceURL(rootpath, id)
+}
+
